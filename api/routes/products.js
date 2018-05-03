88cef4afdd81cbf2078f544f8a6ae2e7e8a96498
Sahadeb Barman <saha.ptc.om@gmail.com>
2018-05-03 12:51:21 +0530

    first commit

---
+++ api/routes/products.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+router.get('/',(req,res,next)=>{
+    res.status(200).json({
+        message:'Handling GET request to .products'
+    });
+});
+
+router.post('/',(req,res,next)=>{
+    res.status(200).json({
+        message:'Handling POST request to .products'
+    });
+});
+module.exports= router;
+//comment3333333333333333333333333333333333333
